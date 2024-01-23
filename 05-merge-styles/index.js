a3b50d8ebf8e4ad3cb2aba06491002df73b1e488
Filipp Kadushkin <kadushkin.feniks@gmail.com>
2024-01-23 04:32:23 +0300

    feat: implement 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const stylesDir = path.join(__dirname, 'styles');
+const distDir = path.join(__dirname, 'project-dist');
+const bundleFile = path.join(distDir, 'bundle.css');
+
+async function mergeStyles() {
+  try {
+    await fs.mkdir(distDir, { recursive: true });
+
+    const styleFiles = await fs.readdir(stylesDir);
+    const filteredCssFiles = styleFiles.filter(
+      (file) => path.extname(file) === '.css',
+    );
+
+    for (const cssFile of filteredCssFiles) {
+      const filePath = path.join(stylesDir, cssFile);
+      const fileContent = await fs.readFile(filePath, 'utf8');
+      await fs.appendFile(bundleFile, fileContent, 'utf8');
+    }
+    console.log('Поздравляю! Стили успешно объединены.');
+  } catch (error) {
+    console.log(`Ошибка при объединении стилей: ${error.message}`);
+  }
+}
+
+mergeStyles();
